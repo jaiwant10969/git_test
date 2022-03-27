785d91a5b90196602b2e09b202c683eff92487ad
Jaiwant <jaiwant.r@gmail.com>
2022-03-27 17:29:40 +0530

    Reverse a LL - Iterative method

---
+++ LL/LL6.cpp
@@ -0,0 +1,64 @@
+//Reverse a LL - Iterative method
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node *next;
+};
+
+Node *head;
+
+void Insert(int x){
+    Node *temp = new Node();
+    temp->data=x;
+    temp->next=NULL;
+    if(head==NULL){
+        head=temp;
+        return;
+    }
+    Node *temp2 = head;
+    while(temp2->next!=NULL){
+        temp2=temp2->next;
+    }
+    temp2->next=temp;
+}
+
+void Reverse(){
+    Node *prev,*current,*next;
+    current=head;
+    prev=NULL;
+    while(current!=NULL){
+        next=current->next;
+        current->next=prev;
+        prev=current;
+        current=next;
+    }
+    head=prev;
+}
+
+void Print(){
+    Node *temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<"\n";
+}
+
+int main(){
+    head=NULL;
+    int n,x;
+    cout<<"How many elements: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"Enter the number: ";
+        cin>>x;
+        Insert(x);
+        Print();
+    }
+    Reverse();
+    cout<<"Reversed List: ";
+    Print();
+    return 0;
+}
